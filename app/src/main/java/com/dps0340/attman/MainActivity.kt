b0807304af4d0c6d0519c032e5dea4449146c4a8
jiho lee <jiholee@kpu.ac.kr>
2020-10-25 21:47:15 +0900

    Add: invoke LoginActivity after 2 sec from onCreate

--- app/src/main/java/com/dps0340/attman/MainActivity.kt
+++ app/src/main/java/com/dps0340/attman/MainActivity.kt
@@ -2,22 +2,25 @@ package com.dps0340.attman
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.MotionEvent
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(MyView(this))
-        //setContentView(R.layout.activity_main);
+        Handler(Looper.getMainLooper()).postDelayed({
+            this.onClick(null)
+            Log.i("MAINACTIVITY", "set onclick after 2 sec")
+        }, 2000)
     }
 
-    override fun onTouchEvent(event: MotionEvent): Boolean { //터치시 다음화면인 로그인화면으로 이동
-        val action = event.action
-        if (action == MotionEvent.ACTION_DOWN) {
-            val intent = Intent(this@MainActivity, LoginActivity::class.java)
-            startActivity(intent)
-        }
-        return super.onTouchEvent(event)
+    override fun onClick(view: View?) {
+        Log.i("MAINACTIVITY", "start LoginActivity")
+        val intent = Intent(this@MainActivity, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
